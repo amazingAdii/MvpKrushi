2e4f7ea2c22feea356e81397a5872f67f93b3bb1
Aditya Marathe <aditya33.am@gmail.com>
2019-03-20 00:33:34 +0530

    Changes with image upload
    admin-selling-list.html is new html file for uploading files don't use MVPkrushiBasket.html

--- index.js
+++ index.js
@@ -1,23 +1,80 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDlERu9deNlB2ZBo7LtcNkzUibYPq4z_X8",
+  authDomain: "mvp-krushi-basket.firebaseapp.com",
+  databaseURL: "https://mvp-krushi-basket.firebaseio.com",
+  projectId: "mvp-krushi-basket",
+  storageBucket: "mvp-krushi-basket.appspot.com",
+  messagingSenderId: "116321154537"
+};
+firebase.initializeApp(config);
+
+const database = firebase.database().ref();
+const storageRef = firebase.storage().ref();
+
 function submitHere(){
   var name = document.getElementById('email').value;
   var price = document.getElementById('price').value;
   var quanti = document.getElementById('Qt').value;
+  var sPrice = document.getElementById('sPrice').value;
+  var selectedFile = document.getElementById('get-image').files[0];
  
+  if(name!="" || price!="" ||quanti!="" ||sPrice!=""){
 
-  var database = firebase.database().ref();
- 
-  database.push().set({
-    price : price,
-    Product : name,
-    quantityAvi : quanti
+    //document.querySelector('.file-select').addEventListener('change', handleFileUploadChange);
+    //document.querySelector('.file-submit').addEventListener('click', handleFileUploadSubmit);
+    if(selectedFile!=null) {
+      console.log('function call upload file');
+      handleFileUploadSubmit(name, price, sPrice, quanti, selectedFile);
+    }else{
+      console.log('file is null');
+    }
+
+  } else {
+    //Handle popup for fields empty
+    console.log('Fields are empty')
+  }
+
+}
+
+// function handleFileUploadChange(e) {
+//   selectedFile = e.target.files[0];
+// }
+
+function handleFileUploadSubmit(name, price, sPrice,quanti, selectedFile) {
+
+  //Uploading seleted file to firebase storage
+  const uploadTask = storageRef.child(`farmer_buy_list/${selectedFile.name}`).put(selectedFile);
+  
+  uploadTask.on('state_changed', (snapshot) =>{
+    //show progress bar whikle uploading the task
+    console.log('Uplaoding Image file');
+  },
+   (error) => {
+    //Show error here while uploading the task
+    console.log(error);
+  },
+   () => {
+    //Show succesfull message here and do stuff
+    console.log('Success');
+    var imageRef = storageRef.child(`farmer_buy_list/${selectedFile.name}`);
+    imageRef.getDownloadURL().then(function(url){
+      //use download url for further process
+      database.push().set({
+        price : price,
+        Product : name,
+        quantityAvi : quanti,
+        sPrice : sPrice,
+        imageUrl : url
+      });
+    }).catch(function(error){
+      //show error while getting download url
+      console.log(error);
+    })      
   });
 
-
 }
 
 
-function myFunction() {
-  var x = document.createElement("INPUT");
-  x.setAttribute("type", "file");
-  document.body.appendChild(x);
-}
+
+
